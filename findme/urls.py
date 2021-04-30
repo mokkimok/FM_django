124d8556d024711ccae539d35de925f4dbbb8e57
nataegorova <natanatster@gmail.com>
2021-04-30 12:48:35 +0300

    Add location /api/ in nginx.conf

--- findme/urls.py
+++ findme/urls.py
@@ -5,10 +5,10 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('posts.urls')),
-    path('api/v1/base_auth/', include('rest_framework.urls')),
-    path('api/v1/auth/', include('djoser.urls')),
-    path('api/v1/auth_token/', include('djoser.urls.authtoken')),
+    path('/', include('posts.urls')),
+    path('base_auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth_token/', include('djoser.urls.authtoken')),
 ]
 
 if settings.DEBUG:
